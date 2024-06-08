da55459d21bb1706f63977a62a2a6d99da22dd1a
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2024-06-08 17:24:30 +0200

    Fackoff calendar

--- scripts.js
+++ scripts.js
@@ -1,29 +1,34 @@
-// Отримуємо елементи модального вікна
-var modal = document.getElementById("ticket-modal");
-var btn = document.getElementById("buy-ticket-btn");
-var span = document.getElementsByClassName("close")[0];
+// script.js
 
-// Коли користувач натискає кнопку, відкривається модальне вікно
-btn.onclick = function() {
-    modal.style.display = "block";
-    // Ініціалізація календаря Pikaday при відкритті модального вікна
-    new Pikaday({
-        field: document.getElementById('calendar-input'),
-        format: 'D MMM YYYY',
-        onSelect: function() {
-            console.log(this.getMoment().format('Do MMMM YYYY'));
-        }
-    });
-}
+document.addEventListener('DOMContentLoaded', function() {
+  // Отримуємо елементи модального вікна
+  var modal = document.getElementById("ticket-modal");
+  var btn = document.getElementById("buy-ticket-btn");
+  var span = document.getElementsByClassName("close")[0];
 
-// Коли користувач натискає на <span> (x), закривається модальне вікно
-span.onclick = function() {
-    modal.style.display = "none";
-}
+  // Коли користувач натискає кнопку, відкривається модальне вікно
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
 
-// Коли користувач натискає будь-де поза модальним вікном, закривається модальне вікно
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  // Коли користувач натискає на <span> (x), закривається модальне вікно
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // Коли користувач натискає будь-де поза модальним вікном, закривається модальне вікно
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  // Обробка натискання кнопок дегустацій
+  document.getElementById('tasting1').onclick = function() {
+      alert('Вибрано Дегустацію 1');
+  }
+
+  document.getElementById('tasting2').onclick = function() {
+      alert('Вибрано Дегустацію 2');
+  }
+});
