ac46d16ff75dd1ba729dd9b16c4e0e8392587061
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2024-06-08 16:46:39 +0200

    modal p

--- scripts.js
+++ scripts.js
@@ -1,63 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      initialDate: '2024-06-07',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2024-06-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2024-06-07',
-          end: '2024-06-10'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2024-06-09T16:00:00'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2024-06-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2024-06-11',
-          end: '2024-06-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2024-06-12T10:30:00',
-          end: '2024-06-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2024-06-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2024-06-12T14:30:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2024-06-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'https://google.com/',
-          start: '2024-06-28'
+// Отримуємо елементи модального вікна
+var modal = document.getElementById("ticket-modal");
+var btn = document.getElementById("buy-ticket-btn");
+var span = document.getElementsByClassName("close")[0];
+
+// Коли користувач натискає кнопку, відкривається модальне вікно
+btn.onclick = function() {
+    modal.style.display = "block";
+    // Ініціалізація календаря Pikaday при відкритті модального вікна
+    new Pikaday({
+        field: document.getElementById('calendar-input'),
+        format: 'D MMM YYYY',
+        onSelect: function() {
+            console.log(this.getMoment().format('Do MMMM YYYY'));
         }
-      ]
     });
-  
-    calendar.render();
-  });
+}
+
+// Коли користувач натискає на <span> (x), закривається модальне вікно
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Коли користувач натискає будь-де поза модальним вікном, закривається модальне вікно
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
