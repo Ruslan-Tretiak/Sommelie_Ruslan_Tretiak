fab09a9b75002ed56a5f25b5d012076ee6073ed5
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2024-06-21 18:05:52 +0200

    burger

--- scripts.js
+++ scripts.js
@@ -1,34 +1,48 @@
-// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Burger Menu Script
+    const burgerMenu = document.getElementById('burger-menu');
+    const modalMenu = document.getElementById('modal-menu');
+    const closeMenu = document.querySelector('.close-menu');
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Отримуємо елементи модального вікна
-  var modal = document.getElementById("ticket-modal");
-  var btn = document.getElementById("buy-ticket-btn");
-  var span = document.getElementsByClassName("close")[0];
+    burgerMenu.addEventListener('click', function () {
+        modalMenu.style.display = 'block';
+    });
 
-  // Коли користувач натискає кнопку, відкривається модальне вікно
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
+    closeMenu.addEventListener('click', function () {
+        modalMenu.style.display = 'none';
+    });
 
-  // Коли користувач натискає на <span> (x), закривається модальне вікно
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
+    window.addEventListener('click', function (event) {
+        if (event.target == modalMenu) {
+            modalMenu.style.display = 'none';
+        }
+    });
 
-  // Коли користувач натискає будь-де поза модальним вікном, закривається модальне вікно
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
+    // Tasting Event Script
+    var tastingEvents = document.querySelectorAll('.tasting-event');
 
-  // Обробка натискання кнопок дегустацій
-  document.getElementById('tasting1').onclick = function() {
-      alert('Вибрано Дегустацію 1');
-  }
+    tastingEvents.forEach(function(event) {
+        event.addEventListener('click', function() {
+            window.location.href = 'get-your-ticket.html?id=' + event.id;
+        });
+    });
 
-  document.getElementById('tasting2').onclick = function() {
-      alert('Вибрано Дегустацію 2');
-  }
+    // Modal Script
+    var modal = document.getElementById("ticket-modal");
+    var btn = document.getElementById("buy-ticket-btn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
